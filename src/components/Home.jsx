4012c5e6578395735a31494ba7f2958ffb50ade1
Anubhav Singh <anubhavssingh177@gmail.com>
2020-01-27 01:15:33 +0530

    web ki jhaat
    do log bne web ki jhaat

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 export class Home extends Component {
   render() {
     return (
-      <div class="how-section1" style={{ marginTop: "70px",padding-left: "30px" }}>
+      <div class="how-section1" style={{ marginTop: "10px" }}>
+       
         <div class="row">
           <div class="col-lg-6 how-img">
-            <img src="https://image.ibb.co/dDW27U/Work_Section2_freelance_img1.png" class="rounded-circle img-fluid" alt="" />
+            <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.wsimag.com%2Fattachments%2F062bc8c28b7ef8f9cd625e660ad26e3916e4f503%2Fstore%2Ffill%2F1230%2F692%2F27d9a58d2d32b31326bc99cba7f4c9794fb7b670c903f48f3d1222fbcd93%2FWhy-Do-Some-Men-Rape.jpg&imgrefurl=https%3A%2F%2Fwsimag.com%2Feconomy-and-politics%2F31084-why-do-some-men-rape&docid=eXB0DANP9fn6yM&tbnid=C_1OgLaqGtXoAM%3A&vet=10ahUKEwjn1oqV_KHnAhUBb30KHZo0B1YQMwg8KAUwBQ..i&w=1230&h=692&safe=strict&client=safari&bih=662&biw=1252&q=rape%20&ved=0ahUKEwjn1oqV_KHnAhUBb30KHZo0B1YQMwg8KAUwBQ&iact=mrc&uact=8" class="rounded-circle img-fluid" alt="" />
           </div>
           <div class="col-lg-6">
             <h2>Rape</h2>
@@ -16,14 +17,15 @@ export class Home extends Component {
         
         <div class="row">
           <div class="col-lg-6 how-img">
-            <img src="https://image.ibb.co/ctSLu9/Work_Section2_freelance_img3.png" class="rounded-circle img-fluid" alt="" />
+            <img src="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjRyaPR-6HnAhXEzjgGHa_tD0QQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.naidunia.com%2Fchhattisgarh%2Fraipur-daughterinlaw-out-of-the-house-due-to-demand-of-dowry-in-chhattisgarh-2785277&psig=AOvVaw3BwbeCssJM7otvJ42b3QzS&ust=1580152135651646" class="rounded-circle img-fluid" alt="" />
           </div>
           <div class="col-lg-6">
             <h4>Dowry</h4>
             <h4 class="subheading">Dowries are considered a major contributor towards violence against women in India. Some of these offences include physical violence, emotional abuses, and murder of brides and girls</h4>
             <p class="text-muted">Most dowry deaths occur when the young woman, unable to bear the harassment and torture, commits suicide. Most of these suicides are by hanging, poisoning or by fire. Sometimes the woman is killed by setting her on fire - this is known as bride burning, and is sometimes disguised as suicide or accident.[20] In 2012, 8,233 dowry death cases were reported across India.[21] Dowry issues caused 1.4 deaths per year per 100,000 women in India</p>
           </div>
-        </div>
+		</div>
+
         <div class="row">
           <div class="col-lg-6">
             <h4>Domestic violence</h4>
@@ -31,10 +33,37 @@ export class Home extends Component {
             <p class="text-muted">The National Crime Records Bureau reveal that a crime against a woman is committed every three minutes, a woman is raped every 29 minutes, a dowry death occurs every 77 minutes, and one case of cruelty committed by either the husband or relative of the husband occurs every nine minutes.This occurs despite the fact that women in India are legally protected from domestic abuse under the Protection of Women from Domestic Violence Act</p>
           </div>
           <div class="col-lg-6 how-img">
-            <img src="https://image.ibb.co/gQ9iE9/Work_Section2_freelance_img4.png" class="rounded-circle img-fluid" alt="" />
+            <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.focusforhealth.org%2Fwp-content%2Fuploads%2F2019%2F10%2F706FCC50-8833-4DE1-A3C8-C04B0950ED12.jpeg&imgrefurl=https%3A%2F%2Fwww.focusforhealth.org%2Fthe-role-of-patriarchy-in-domestic-violence%2F&docid=nR5oFjo5ap-krM&tbnid=OvlCeI1zCtv-KM%3A&vet=10ahUKEwjhnZvM_KHnAhWCXCsKHfsyA7cQMwh0KB0wHQ..i&w=460&h=232&safe=strict&client=safari&bih=662&biw=1252&q=Domestic%20violence&ved=0ahUKEwjhnZvM_KHnAhWCXCsKHfsyA7cQMwh0KB0wHQ&iact=mrc&uact=8" class="rounded-circle img-fluid" alt="" />
           </div>
         </div>
-      </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <h4>Cyber crime</h4>
+            <h4 class="subheading">The Information Technology Act 2000 was passed by the Parliament of India in May 2000, aiming to curb cyber crimes and provide a legal framework for e-commerce transactions.[50] However Pavan Duggal, lawyer of Supreme Court of India and cyber law expert, viewed "The IT Act, 2000, is primarily meant to be a legislation to promote e-commerce.</h4>
+            <p class="text-muted">It is not very effective in dealing with several emerging cyber crimes like cyber harassment, defamation, stalking and so on". Although cyber crime cells have been set up in major cities, Duggal noted the problem is that most cases remain unreported due to a lack of awareness. In 2001, India and United States had set up an India-US cyber security forum as part of a counter-terrorism dialogue</p>
+          </div>
+          <div class="col-lg-6 how-img">
+            <img src="https://www.google.com/calendar?tab=ic1&authuser=0" class="rounded-circle img-fluid" alt="" />
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <h4>Illegal drug trade</h4>
+            <h4 class="subheading">India is located between two major illicit opium producing centres in Asia – the Golden Crescent comprising Pakistan, Afghanistan and Iran and the Golden Triangle comprising Burma, Thailand and Laos. Because of such geographical location, India experiences large amount of drug trafficking through the borders. India is the world's largest producer of licit opium for the pharmaceutical trade. But an undetermined quantity of opium is diverted to illicit international drug markets.
+
+			</h4>
+            <p class="text-muted">India is a transshipment point for heroin from Southwest Asian countries like Afghanistan and Pakistan and from Southeast Asian countries like Burma, Laos, and Thailand.[30] Heroin is smuggled from Pakistan and Burma, with some quantities transshipped through Nepal.[30] Most heroin shipped from India are destined for Europe.[30] There have been reports of heroin smuggled from Mumbai to Nigeria for further export. In Maharashtra, Mumbai is an important centre for distribution of drug.[31] The most commonly used drug in Mumbai is Indian heroin (called desi mal by the local population). Both public transportation (road and rail transportation) and private transportation are used for this drug trade.
+				</p>
+          </div>
+          <div class="col-lg-6 how-img">
+            <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fbrocku.ca%2Fbrock-news%2Fwp-content%2Fuploads%2F2018%2F04%2FIllicit-drugs-1600x1067.jpg%3Fx68208&imgrefurl=https%3A%2F%2Fbrocku.ca%2Fbrock-news%2F2018%2F04%2Fnew-course-takes-close-look-at-illegal-drug-trade%2F&docid=g8Sgpb-ZGu6HFM&tbnid=k_4Vd1NjedetPM%3A&vet=10ahUKEwjt-aemgaLnAhVZeysKHZnIAooQMwhEKAYwBg..i&w=1600&h=1067&safe=strict&client=safari&bih=727&biw=1252&q=Illegal%20drug%20trade&ved=0ahUKEwjt-aemgaLnAhVZeysKHZnIAooQMwhEKAYwBg&iact=mrc&uact=8" class="rounded-circle img-fluid" alt="" />
+          </div>
+        </div>
+
+</div>
+      
     )
   }
 }
